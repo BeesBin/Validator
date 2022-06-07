d641c388e08fb721fbda150aa28d4bfbe7eee6aa
DESKTOP-O7S2749\LG CNS <Hyunbin.Gwak@lgcns.com>
2022-06-07 21:33:10 +0900

    초기 커밋.

---
+++ ValidatorProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidatorProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
